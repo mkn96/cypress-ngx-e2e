70b7388674a37b18ef8b630ab910b966768e3154
mkn <michael.stieblov@gmail.com>
2023-01-19 20:58:45 +0100

    json

---
+++ cypress/e2e/jsonObjects.spec.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+describe('JSON objects', () => {
+    it('JSON objects', () => {
+        cy.openHomePage()
+
+        const simpleObject = { "key": "value", "key2": "value2" }
+        
+        const simpleArrayOfValues = [ "one", "two", "three"]
+
+        const arrayOfObjects = [{"key": "valure"}, {"key2": "value2"}, {"key3": "value3"}]
+
+        const typesOfData = { "string": "this is a string", "number": 10}
+
+        const mix =  {
+            "FirstName": "Mykhail",
+            "LastName": "Stieblov",
+            "Age": 25,
+            "Students": [
+                {
+                    "firstName": "Sara",
+                    "lastName": "Conor",
+                },
+                {
+                    "firstName": "Bruce",
+                    "lastName": "Willis"
+                }
+            ]
+        }
+
+        console.log(simpleObject.key2)
+        console.log(simpleObject["key2"])
+        console.log(simpleArrayOfValues[1])
+        console.log(arrayOfObjects[2].key3)
+        console.log(mix.Students[1].lastName)
+
+
+        const lastNameOfSecondStudent = mix.Students[1].lastName
+
+
+})
+
+
+})
